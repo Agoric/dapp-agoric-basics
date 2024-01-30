a2a76e2082ab9ae5243c890f0de378f77479c4f4
Luqi Pan <luqi@agoric.com>
2024-01-30 13:03:49 -0800

    feat: add test for greet function in contract

---
+++ contract/test/test-agoric-basics-contract.js
@@ -0,0 +1,13 @@
+// @ts-check
+import '@endo/init';
+import { E } from '@endo/far';
+// eslint-disable-next-line import/no-unresolved -- https://github.com/avajs/ava/issues/2951
+import test from 'ava';
+
+import { start } from '../src/agoric-basics-contract.js';
+
+test('contract greets by name', async t => {
+  const { publicFacet } = start();
+  const actual = await E(publicFacet).greet('Bob');
+  t.is(actual, 'Hello, Bob!');
+});
