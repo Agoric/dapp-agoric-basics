f06fbd28a538779ef233175c3f7d41649c77e2c5
Luqi Pan <luqi@agoric.com>
2024-02-21 17:15:59 -0800

    feat: add component for Mint and Inventory

---
+++ interchain-ui/src/components/Inventory.tsx
@@ -0,0 +1,53 @@
+import { ConnectWalletButton, useAgoric } from '@agoric/react-components';
+import { stringifyAmountValue } from '@agoric/ui-components';
+import { usePurse } from '../hooks/usePurse';
+import type { CopyBag } from '../types';
+
+const Inventory = () => {
+  const istPurse = usePurse('IST');
+  const itemsPurse = usePurse('Tickets');
+  const { walletConnection } = useAgoric();
+
+  return (
+    <div className="card">
+      <h3>My Wallet</h3>
+      <div>
+        <ConnectWalletButton />
+        {walletConnection && (
+          <div style={{ textAlign: 'left' }}>
+            <div>
+              <b>IST: </b>
+              {istPurse ? (
+                stringifyAmountValue(
+                  istPurse.currentAmount,
+                  istPurse.displayInfo.assetKind,
+                  istPurse.displayInfo.decimalPlaces,
+                )
+              ) : (
+                <i>Fetching balance...</i>
+              )}
+            </div>
+            <div>
+              <b>Items: </b>
+              {itemsPurse ? (
+                <ul style={{ marginTop: 0, textAlign: 'left' }}>
+                  {(itemsPurse.currentAmount.value as CopyBag).payload.map(
+                    ([name, number]) => (
+                      <li key={name}>
+                        {String(number)} {name}
+                      </li>
+                    ),
+                  )}
+                </ul>
+              ) : (
+                'None'
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export { Inventory };
---
+++ interchain-ui/src/components/Mint.tsx
@@ -0,0 +1,55 @@
+import { Box, NftMint } from '@interchain-ui/react';
+import { ThemeProvider, useTheme } from '@interchain-ui/react';
+import '@interchain-ui/react/styles';
+
+const MintConcertTicket = ({
+  kind,
+  price,
+}: {
+  kind: string;
+  price: number;
+}) => {
+  return (
+    <Box>
+      <NftMint
+        tag="NOW LIVE"
+        title="Mint"
+        name={kind + ' Row'}
+        description={kind + ' row concert ticket'}
+        quantity={3}
+        royalties={0}
+        minted={0}
+        available={100}
+        priceDisplayAmount={price}
+        limited={3}
+        tokenName="IST"
+        imgSrc="src/assets/react.svg"
+        pricePerToken={price}
+        onMint={() => {
+          console.log('onMint');
+        }}
+        onChange={(value: number) => {
+          console.log('onChange', value);
+        }}
+      />
+    </Box>
+  );
+};
+
+const Mint = () => {
+  const { themeClass } = useTheme();
+
+  return (
+    <>
+      <ThemeProvider>
+        <div className={themeClass}>
+          <MintConcertTicket kind="Front" price={3} />
+          <MintConcertTicket kind="Middle" price={2} />
+          <MintConcertTicket kind="Last" price={1} />
+        </div>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export { Mint };
