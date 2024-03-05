df9929f1b4550b531fc72ed965791d46fbe39599
Dan Connolly <dckc@madmode.com>
2024-03-05 17:26:51 -0600

    WIP(deploy-contract): CLI stuff

--- contract/scripts/deploy-contract.js
+++ contract/scripts/deploy-contract.js
@@ -8,10 +8,43 @@ import { execFile, execFileSync } from 'node:child_process';
 import { makeNodeBundleCache } from '@endo/bundle-source/cache.js';
 import { makeE2ETools } from '../tools/e2e-tools.js';
 
+/**
+ * @template T
+ * @param {T | null | undefined } x
+ * @returns {T}
+ */
+const NonNullish = x => {
+  if (x === undefined || x === null) throw assert.error('NonNullish');
+  return x;
+};
+
+/**
+ * @param {string[]} args
+ * @param {{ [k: string]: boolean | undefined }} [style]
+ */
+const getopts = (args, style = {}) => {
+  /** @type {{ [k: string]: string}} */
+  const flags = {};
+  while (args.length > 0) {
+    const arg = NonNullish(args.shift());
+    if (arg.startsWith('--')) {
+      const name = arg.slice('--'.length);
+      if (style[name] === true) {
+        flags[name] = '';
+        continue;
+      }
+      if (args.length <= 0) throw RangeError(`no value for ${arg}`);
+      flags[name] = NonNullish(args.shift());
+    }
+  }
+  return harden(flags);
+};
+
 const main = async (bundleDir = 'bundles') => {
   const { argv, env } = process;
   const { writeFile } = fsp;
 
+  const opts = getopts(argv.slice(2));
   const bundleCache = makeNodeBundleCache(bundleDir, {}, s => import(s));
 
   const t = {
