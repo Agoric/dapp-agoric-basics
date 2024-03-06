705b425220745fe6f92a4b64f4ab2bbf872f93ab
Dan Connolly <dckc@madmode.com>
2024-03-06 02:23:53 -0600

    chore(rollup.config): both sell and pay contracts

--- contract/rollup.config.mjs
+++ contract/rollup.config.mjs
@@ -16,25 +16,57 @@ import {
   configureBundleID,
   emitPermit,
 } from './tools/rollup-plugin-core-eval.js';
-import { permit } from './src/postal-service.proposal.js';
+import { permit as postalServicePermit } from './src/postal-service.proposal.js';
+import { permit as sellPermit } from './src/sell-concert-tickets-proposal.js';
 
-/** @type {import('rollup').RollupOptions} */
-const config = {
+/**
+ * @param {*} opts
+ * @returns {import('rollup').RollupOptions}
+ */
+const config1 = ({
+  name,
+  coreEntry,
+  coreScript,
+  contractEntry,
+  permit,
+  permitFile,
+}) => ({
+  input: coreEntry,
   output: {
     globals: coreEvalGlobals,
-    file: 'bundles/deploy-postal-service.js',
+    file: coreScript,
     format: 'es',
     footer: 'main',
   },
   external: ['@endo/far'],
   plugins: [
     configureBundleID({
-      name: 'postalService',
-      rootModule: './src/postal-service.contract.js',
+      name,
+      rootModule: contractEntry,
       cache: 'bundles',
     }),
     moduleToScript(),
-    emitPermit({ permit, file: 'deploy-postal-service-permit.json' }),
+    emitPermit({ permit, file: permitFile }),
   ],
-};
+});
+
+/** @type {import('rollup').RollupOptions[]} */
+const config = [
+  config1({
+    name: 'postalService',
+    coreEntry: './src/postal-service.proposal.js',
+    coreScript: 'bundles/deploy-postal-service.js',
+    contractEntry: './src/postal-service.contract.js',
+    permit: postalServicePermit,
+    permitFile: 'deploy-postal-service-permit.json',
+  }),
+  config1({
+    name: 'sell-concert-tickets',
+    coreEntry: './src/sell-concert-tickets-proposal.js',
+    coreScript: 'bundles/deploy-sell-concert-tickets.js',
+    contractEntry: './src/sell-concert-tickets.contract.js',
+    permit: sellPermit,
+    permitFile: 'deploy-sell-concert-tickets-permit.json',
+  }),
+];
 export default config;
