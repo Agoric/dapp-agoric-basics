bdbec11dc1de1567d4920a7a4400b3d65647ac51
Dan Connolly <dckc@madmode.com>
2024-03-06 10:08:43 -0600

    feat(start-contract): publish issuers, brands

--- contract/src/platform-goals/start-contract.js
+++ contract/src/platform-goals/start-contract.js
@@ -25,11 +25,15 @@ export const installContract = async (
  * @param {{
  *   name: string;
  *   startArgs?: StartArgs;
+ *   issuerNames?: string[];
  * }} opts
  *
  * @typedef {Partial<Parameters<Awaited<BootstrapPowers['consume']['startUpgradable']>>[0]>} StartArgs
  */
-export const startContract = async (powers, { name, startArgs }) => {
+export const startContract = async (
+  powers,
+  { name, startArgs, issuerNames },
+) => {
   const {
     consume: { startUpgradable },
     installation: { consume: consumeInstallation },
@@ -49,5 +53,32 @@ export const startContract = async (powers, { name, startArgs }) => {
   produceInstance[name].resolve(instance);
 
   console.log(name, 'started');
+
+  if (issuerNames) {
+    /** @type {BootstrapPowers & import('./boardAux').BoardAuxPowers} */
+    // @ts-expect-error cast
+    const auxPowers = powers;
+
+    const { zoe, brandAuxPublisher } = auxPowers.consume;
+    const { produce: produceIssuer } = auxPowers.issuer;
+    const { produce: produceBrand } = auxPowers.brand;
+    const { brands, issuers } = await E(zoe).getTerms(instance);
+
+    await Promise.all(
+      issuerNames.map(async issuerName => {
+        const brand = brands[issuerName];
+        const issuer = issuers[issuerName];
+        console.log('CoreEval script: share via agoricNames:', brand);
+
+        produceBrand[issuerName].reset();
+        produceIssuer[issuerName].reset();
+        produceBrand[issuerName].resolve(brand);
+        produceIssuer[issuerName].resolve(issuer);
+
+        await E(brandAuxPublisher).publishBrandInfo(brand);
+      }),
+    );
+  }
+
   return started;
 };
