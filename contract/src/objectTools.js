b8acff06a3af032b529b498d1de794d9d5d5ecd0
Dan Connolly <dckc@madmode.com>
2024-03-09 07:46:08 -0600

    docs(objectTools): describe functions

--- contract/src/objectTools.js
+++ contract/src/objectTools.js
@@ -3,7 +3,11 @@ import { E } from '@endo/far';
 
 const { entries, fromEntries } = Object;
 
-/** @type { <T extends Record<string, ERef<any>>>(obj: T) => Promise<{ [K in keyof T]: Awaited<T[K]>}> } */
+/**
+ * Given a record whose values may be promise, return a promise for a record with all the values resolved.
+ *
+ * @type { <T extends Record<string, ERef<any>>>(obj: T) => Promise<{ [K in keyof T]: Awaited<T[K]>}> }
+ */
 export const allValues = async obj => {
   const es = await Promise.all(
     entries(obj).map(([k, vp]) => E.when(vp, v => [k, v])),
@@ -11,7 +15,11 @@ export const allValues = async obj => {
   return fromEntries(es);
 };
 
-/** @type { <V, U, T extends Record<string, V>>(obj: T, f: (v: V) => U) => { [K in keyof T]: U }} */
+/**
+ * Map a function over the values of a record.
+ *
+ * @type { <V, U, T extends Record<string, V>>(obj: T, f: (v: V) => U) => { [K in keyof T]: U }}
+ */
 export const mapValues = (obj, f) =>
   fromEntries(
     entries(obj).map(([p, v]) => {
@@ -20,10 +28,16 @@ export const mapValues = (obj, f) =>
     }),
   );
 
-/** @type {<X, Y>(xs: X[], ys: Y[]) => [X, Y][]} */
+/**
+ * Given 2 arrays that form columns of a table, return an array of the rows.
+ *
+ * @type {<X, Y>(xs: X[], ys: Y[]) => [X, Y][]}
+ */
 export const zip = (xs, ys) => xs.map((x, i) => [x, ys[i]]);
 
 /**
+ * Dynamic check for non-nullish
+ *
  * @template T
  * @param {T | null | undefined } x
  * @returns {T}
