58321eb29194eb4d79ed7d53f373d1cca80a3dad
mudassir-agoric <mudassir@agoric.com>
2024-05-15 17:08:41 +0500

    fix: zcf is being used now

--- contract/src/cateCoin.contract.js
+++ contract/src/cateCoin.contract.js
@@ -1,77 +1,77 @@
-
 import { AmountMath, makeIssuerKit } from '@agoric/ertp';
 import { Far } from '@endo/far';
 
 import '@agoric/zoe/exported.js';
 
-
-
-export const start = async (zcf) => {
-
+export const start = async zcf => {
   // Step 0: get an issuer kit
   const {
     issuer: cateIssuer,
     mint: cateMint,
     brand: cateBrand,
   } = makeIssuerKit('cateCoin');
-  let currSupply = AmountMath.make(cateBrand, 0n);;
-  const maxSupply = AmountMath.make(cateBrand, 1000_000n);;
+  let currSupply = AmountMath.make(cateBrand, 0n);
+  const maxSupply = AmountMath.make(cateBrand, 1000_000n);
   let creatorPurse;
   let isInitialized = 0;
 
   const getIssuer = () => cateIssuer;
+  const cateAssetKind = zcf.getAssetKind(cateBrand);
+  console.log('AssetKind for Cate Coin is : ', cateAssetKind);
 
-  const createInitialCoins = ( myPurse, amount) => {
-
-    if(isInitialized === 1)
-      return 'Fail - already initialized';
+  const createInitialCoins = (myPurse, amount) => {
+    if (isInitialized === 1) return 'Fail - already initialized';
 
     console.log('creating first coins');
     isInitialized = 1;
     creatorPurse = myPurse;
-    
+
     const cateAmount = AmountMath.make(cateBrand, amount);
-    if (AmountMath.isGTE(cateAmount, maxSupply ) )
+    if (AmountMath.isGTE(cateAmount, maxSupply))
       return 'fail - amount exceeds maxSupply';
-    const catePayment = cateMint.mintPayment( cateAmount );
-    
+    const catePayment = cateMint.mintPayment(cateAmount);
+
     creatorPurse.deposit(catePayment);
     currSupply = amount;
-  
+
     const currentAmount = creatorPurse.getCurrentAmount();
-    console.log("Current Amount in my purse after deposit is : ", currentAmount);
+    console.log(
+      'Current Amount in my purse after deposit is : ',
+      currentAmount,
+    );
     return 'success';
   };
-  const mintCateCoins = ( myPurse, amount) => {
-    if(creatorPurse !== myPurse)
+  const mintCateCoins = (myPurse, amount) => {
+    if (creatorPurse !== myPurse)
       return 'Fail - Only creator can mint new tokens';
-    if( currSupply+amount > maxSupply )
-      return 'Fail - reached max supply';
-    
+    if (currSupply + amount > maxSupply) return 'Fail - reached max supply';
+
     const cateAmount = AmountMath.make(cateBrand, amount);
-    const catePayment = cateMint.mintPayment( cateAmount );  
+    const catePayment = cateMint.mintPayment(cateAmount);
     creatorPurse.deposit(catePayment);
     currSupply += amount;
-  
+
     return 'success';
   };
-  const transferCateCoins = ( fromPurse, toPurse, amount) => {
-    
+  const transferCateCoins = (fromPurse, toPurse, amount) => {
     const cateAmount = AmountMath.make(cateBrand, amount);
-    if (AmountMath.isGTE(fromPurse.getCurrentAmount(), cateAmount)){
+    if (AmountMath.isGTE(fromPurse.getCurrentAmount(), cateAmount)) {
       const catePayment = fromPurse.withdraw(cateAmount);
       toPurse.deposit(catePayment);
-      return 'success';  
+      return 'success';
     }
     return 'fail - not enough funds in sender account';
   };
 
   return {
-    creatorFacet: Far('Creator Facet', { mintCateCoins, transferCateCoins, getIssuer, createInitialCoins }),
-    publicFacet: Far('Public Facet', {  transferCateCoins }),
+    creatorFacet: Far('Creator Facet', {
+      mintCateCoins,
+      transferCateCoins,
+      getIssuer,
+      createInitialCoins,
+    }),
+    publicFacet: Far('Public Facet', { transferCateCoins }),
   };
-
-
 
   // // Step 1: Get the address of the inviter
   // //Check if this is the first call to this instance of the contract
@@ -80,10 +80,8 @@ export const start = async (zcf) => {
   // if (inviterAddress === undefined) {
   //   zcf.state.inviterAddress = zcf.getAddress();
   //   inviterAddress = zcf.state.get('inviterAddress');
-    
 
   // }
-
 
   // console.log("The address of zoe I got is : ${inviterAddress}");
   // return ;
@@ -92,14 +90,12 @@ export const start = async (zcf) => {
 
   // const inventoryValues = Object.values(inventory);
 
-
   //   const paymentAmount = cateIssuer.getAmountOf(catePayment);
 
   //   //TODO: check - Not sure if we can burn partial amount from a payment
   //   const amountToBurn = AmountMath.make(cateBrand, 5n);
   //   const burntAmount = cateIssuer.burn(catePayment, amountToBurn);
   //   console.log(burntAmount);
-    
 
   //   const catePurse = cateIssuer.makeEmptyPurse();
   //   const currentAmount = catePurse.getCurrentAmount();
@@ -108,11 +104,8 @@ export const start = async (zcf) => {
   //   currentAmount = catePurse.getCurrentAmount();
   //   console.log("Current Amount in my purse after deposit is : ",currentAmount);
 
-
   //   const cate3 = AmountMath.make(brand, 30n);
   //   const withdrawalPayment = purse.withdraw(cate3);
-
-
 };
 
 harden(start);
