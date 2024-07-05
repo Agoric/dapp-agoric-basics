3ffdb937747d315a10f3fdfaa7f9d3ccdc6e825e
Mudassir Shabbir <53943791+amessbee@users.noreply.github.com>
2024-07-05 13:10:47 +0000

    fix: hostname script works on localhost

--- ui/set.hostname.sh
+++ ui/set.hostname.sh
@@ -3,12 +3,6 @@
 # Step 1: Read the environment variable CODESPACE_NAME
 CODESPACE_NAME="${CODESPACE_NAME}"
 
-# Check if the environment variable is set
-if [ -z "$CODESPACE_NAME" ]; then
-  echo "CODESPACE_NAME is not set."
-  exit 1
-fi
-
 # Step 2: Define the file name
 FILE="src/App.tsx"
 
@@ -18,9 +12,17 @@ if [ ! -f "$FILE" ]; then
   exit 1
 fi
 
-# Step 3: Search and replace the string in the file using a regular expression
-sed -i "s|https://.*-1317\.app\.github\.dev/|https://${CODESPACE_NAME}-1317.app.github.dev/|g" "$FILE"
-sed -i "s|https://.*-26657\.app\.github\.dev/|https://${CODESPACE_NAME}-26657.app.github.dev/|g" "$FILE"
+# Step 3: Check if the environment variable is set and replace accordingly
+if [ -z "$CODESPACE_NAME" ]; then
+  echo "CODESPACE_NAME is not set. Setting host with http://localhost"
+  sed -i "s|https://.*-1317\.app\.github\.dev/|http://localhost:1317|g" "$FILE"
+  sed -i "s|https://.*-26657\.app\.github\.dev/|http://localhost:26657|g" "$FILE"
+else
+  echo "CODESPACE_NAME is set to $CODESPACE_NAME. Setting host with https://${CODESPACE_NAME}-1317.app.github.dev/"
+  sed -i "s|https://.*-1317\.app\.github\.dev/|https://${CODESPACE_NAME}-1317.app.github.dev/|g" "$FILE"
+  echo "CODESPACE_NAME is set to $CODESPACE_NAME. Setting host with https://${CODESPACE_NAME}-26675.app.github.dev/"
+  sed -i "s|https://.*-26657\.app\.github\.dev/|https://${CODESPACE_NAME}-26657.app.github.dev/|g" "$FILE"
+fi
 
 # Step 4: Notify the user of completion
-echo "Replaced matching URLs with https://${CODESPACE_NAME}-1317.app.github.dev/ in $FILE"
+echo "Replacement complete in $FILE"
