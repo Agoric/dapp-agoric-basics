119d7ca52f15499e32fd06a4a8f2d1a3d18cb9b0
Mudassir Shabbir <mudassir@agoric.com>
2024-07-23 09:28:07 +0500

    fix: entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -x
 cp -r /usr/src/cache/node_modules /workspaces/dapp-agoric-basics/
 /workspaces/dapp-agoric-basics/make_ports_public.sh 5173 1317 26657 26656
-exec /bin/bash
+# exec /bin/bash
